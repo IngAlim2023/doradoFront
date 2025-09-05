17a91bbfb76eb2a654eee96119e9172d31c7ba25
Jorge Porras <jorgeeporrassandoval@gmail.com>
2025-09-05 11:02:45 -0500

    autenticacion

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,42 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-
+import { useEffect } from "react";
+import useAuthContext from "./context/AuthHook";
 const App = () => {
+  const { isAutenticate, setIsAutenticate } = useAuthContext();
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const response = await fetch("http://localhost:3333/dorado/validate", {
+          method: "GET",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const data = await response.json();
+
+        if (data.message === "Autorizado") {
+          return setIsAutenticate(true);
+        }
+        return setIsAutenticate(false);
+      } catch (e) {
+        return setIsAutenticate(false);
+      }
+    };
+    checkAuth();
+  }, [isAutenticate]);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login/>}/>
-        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
-    </BrowserRouter> 
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,44 @@
-import React from 'react';
-import {useForm} from 'react-hook-form';
+import React, { useEffect } from "react";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import useAuthContext from "../context/AuthHook";
+import { useNavigate } from "react-router-dom";
+type Data={
+    username:string;
+    usernameRequired:string;
+    password:string;
+    passwordRequired:string;
+}
 
-const Login:React.FC = () => {
-    const {register, handleSubmit} = useForm()
+const Login: React.FC = () => {
+  const { register, handleSubmit } = useForm<Data>();
+  const { isAutenticate, setIsAutenticate } = useAuthContext();
+  const navigate = useNavigate();
 
-    const onSubmit = (data) =>{
-        const res = fetch('http://localhost:3333/dorado/login')
+  useEffect(() => {
+    if (isAutenticate) {
+      navigate("/dashboard");
     }
+  }, [isAutenticate]);
+
+  const onSubmit:SubmitHandler<Data> = async (data) => {
+    const res = fetch("http://localhost:3333/dorado/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials:"include",
+      body: JSON.stringify(data),
+    });
+
+    const respuesta = await res;
+
+    if (respuesta.status === 401) return alert("Credenciales incorrectas");
+    if (respuesta.status === 500) return alert("Error vuelve a intentarlo");
+    if (respuesta.status === 200) {
+      setIsAutenticate(true);
+      navigate("/dashboard");
+    }
+  };
   return (
     <div className="h-[75vh] w-screen flex justify-center items-center">
       <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-2xl shadow-lg">
@@ -14,19 +46,24 @@ const Login:React.FC = () => {
           <h3 className="text-center font-bold text-2xl">Iniciar sesión</h3>
         </div>
         <div>
-            <p className="text-gray-500">Ingresa tus credenciales</p>
+          <p className="text-gray-500">Ingresa tus credenciales</p>
         </div>
-        <form className="flex flex-col gap-2 space-y-4" onSubmit={handleSubmit(onSubmit)}>
+        <form
+          className="flex flex-col gap-2 space-y-4"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <input
             type="text"
             className="text-center border-indigo-200 border-b-2 focus:outline-none focus:border-indigo-300"
             placeholder="username"
-            {...register('username', {required:true})}
+            {...register("username", { required: true })}
           />
 
-          <input type="password" className="text-center border-indigo-200 border-b-2 focus:outline-none focus:border-indigo-300" 
-          placeholder="password"
-          {...register('password', {required:true})}
+          <input
+            type="password"
+            className="text-center border-indigo-200 border-b-2 focus:outline-none focus:border-indigo-300"
+            placeholder="password"
+            {...register("password", { required: true })}
           />
           <button className="rounded-lg bg-indigo-600 hover:bg-indigo-800 transition text-white p-1">
             Ingresar
@@ -34,7 +71,7 @@ const Login:React.FC = () => {
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
